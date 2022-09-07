4da6711d0b072ab214a1dace1102c1c833e21b69
glorialunar <glorialunar2509@gmail.com>
2022-09-07 15:45:51 -0300

    Loader y NotFoundMessage

--- src/components/CrudApp.js
+++ src/components/CrudApp.js
@@ -4,20 +4,28 @@ import {v4 as uuid} from 'uuid';
 import { helpHttp } from '../helpers/helpHttp';
 import CrudForm from "./CrudForm"
 import CrudTable from "./CrudTable"
+import Loader from './Loader';
+import Message from './Message';
 
 export default function CrudApp ()  {
-    const [db, setDb] = useState([]);
+    const [db, setDb] = useState(null);
     const [dataToEdit, setDataToEdit] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
     let api = helpHttp();
     let url = "http://localhost:5000/ctd";
 
     useEffect(() => {
+        setLoading(true);
         api.get(url).then((res) =>{
             if(!res.err){
                 setDb(res);
+                setError(null);
             }else{
                 setDb(null);
+                setError(res.err)
             }
+            setLoading(false);
         })
     }, []);
 
@@ -64,11 +72,16 @@ export default function CrudApp ()  {
                 dataToEdit={dataToEdit} 
                 setDataToEdit={setDataToEdit}
             />
-            <CrudTable 
-                data={db}
-                setDataToEdit={setDataToEdit}
-                deleteData={deleteData}
-            />
+
+            {loading && <Loader/>}
+            {error && <Message/>}
+            
+            {db && <CrudTable 
+                        data={db}
+                        setDataToEdit={setDataToEdit}
+                        deleteData={deleteData}
+                    />
+            }    
         </div>
     )
 };
---
+++ src/components/Message.js
@@ -0,0 +1,38 @@
+import tristeza from '../img/tristeza.png';
+import styles from 'styled-components';
+
+const Message = () => {
+    return (
+        <>
+            <MessageContainer>
+                <img src={tristeza} alt="Imagen de tristeza" className='tristeza'/>
+                <section className='info-container'>
+                    <h2>Awwww... don't worry!!</h2>
+                    <p>It's just a 404 Error!</p>
+                </section>
+            </MessageContainer>
+        </>
+    )
+};
+
+export default Message;
+
+const MessageContainer = styles.div`
+    width: 70%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 5rem auto;
+
+    h2{
+        font-size: 1.5rem;
+    }
+
+    .tristeza{
+        width:40%
+    }
+
+    .info-container{
+        text-align: center;
+    }
+`
